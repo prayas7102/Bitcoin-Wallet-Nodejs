40bff14bcc611f5a79fe503965a0157d6965d46b
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-12-29 22:48:32 +0530

    sendbitcoin implemented

--- App.js
+++ App.js
@@ -1,2 +1,12 @@
-const {createWalllet} =require('./wallet');
+const { createWalllet } = require('./wallet');
+const { sendBitcoin } = require('./sendCrypto');
+
+sendBitcoin("mnJJCCLa8FEpVFKoCeE2BUQWKaT6rWUGWV", 0.0001)
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+    
 console.log(createWalllet());
