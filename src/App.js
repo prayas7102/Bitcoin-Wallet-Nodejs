5848f53989072d760b35d03470abb2e92bc6c336
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-12-30 17:58:16 +0530

    docker connection done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const { createWalllet } = require('./wallet');
+const { createWalllet } = require('wallet');
 const { sendBitcoin } = require('./sendCrypto');
 
 sendBitcoin("mnJJCCLa8FEpVFKoCeE2BUQWKaT6rWUGWV", 0.0001)
